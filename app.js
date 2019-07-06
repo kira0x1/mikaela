add3a0470a4938dfe13348da6b681490562404f6
Kira <ahmed_althani@outlook.com>
2019-07-06 10:34:16 +0100

    Show prefix on activity

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const cooldowns = new Discord.Collection()
 
 client.once('ready', () => {
   // process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error))
-  client.user.setActivity('Wholesome propoganda | $help', {
+  client.user.setActivity(`Wholesome propoganda | ${prefix}help`, {
     type: 'WATCHING',
   })
   console.log(`${client.user.username} Online!`)
