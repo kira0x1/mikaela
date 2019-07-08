f0f6de01d96ec03521daba4edf26eb5467ae72c7
Kira <ahmed_althani@outlook.com>
2019-07-08 19:07:42 +0100

    wip music

---
+++ commands/music/play.js
@@ -0,0 +1,15 @@
+const searchOptions = {
+  part: ['snippet', 'contentDetails'],
+  chart: 'mostPopular',
+  maxResults: 1,
+  key: 'AIzaSyAQ_I6nDdIahO4hWerAQ32P4UXFf21_ELo',
+}
+
+
+
+module.exports = {
+  name: 'play',
+  guildOnly: true,
+
+  execute(message, args) {},
+}
---
+++ commands/newMusic.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+
+module.exports = {
+  name: 'music',
+  guildOnly: true,
+
+  execute(message, args) {},
+}
