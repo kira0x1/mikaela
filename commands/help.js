bab59db465d70b83e262fd2f8c6a137a330edca9
Kira <ahmed_althani@outlook.com>
2019-07-08 03:01:55 +0100

    Cleaned up some code

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,5 @@
 const discord = require('discord.js')
 const { prefix } = require('../config.json')
-const sendDM = false
 const { perms, usage } = require('../util/util')
 
 module.exports = {
@@ -30,34 +29,15 @@ module.exports = {
           embed.addField(command.name, command.description)
         }
       })
+    } else {
+      //Send specific command
+      const name = args[0].toLowerCase()
+      const command =
+        commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name))
 
-      if (sendDM) {
-        message.author
-          .send(data, { split: true })
-          .then(() => {
-            if (message.channel.type === 'dm') return
-
-            message.channel.send('I sent you a DM with a list of commands :smile:')
-          })
-          .catch(error => {
-            console.error(`Failed to send help DM to ${message.author.tag}.\n`, error)
-            message.channel.send("Failed to DM you. You might have DM's disabled")
-          })
-      } else if (!sendDM) {
-        message.channel.send(embed).catch(error => {
-          console.error(error)
-        })
+      if (!command) {
+        return message.channel.send(`command not found: ${name}`)
       }
-
-      return
-    }
-
-    //Send specific command
-    const name = args[0].toLowerCase()
-    const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name))
-
-    if (!command) {
-      return message.channel.send(`command not found: ${name}`)
     }
 
     const embedSpecific = new discord.RichEmbed()
@@ -69,13 +49,5 @@ module.exports = {
       .addField('Cooldown', `\`${command.cooldown || 3} second(s)\``)
 
     message.channel.send({ embed: embedSpecific })
-    // data.push(`**Name:** ${command.name}`)
-    // if (command.aliases) data.push(`**Aliases:** ${command.aliases}`);
-    // if (command.description) data.push(`**Description:** ${command.description}`);
-    // data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-    // if (command.usage) data.push(usage(command));
-    // `**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-    // message.channel.send(data, { split: true })
   },
 }
--- commands/lewd.js
+++ commands/lewd.js
@@ -1,55 +1,40 @@
 const agent = require('superagent')
 const { getFlags } = require('../util/util')
 
-const flags = [
-    amount = { name: 'amount', aliases: ['n'] }
-]
+const flags = [(amount = { name: 'amount', aliases: ['n'] })]
 
 module.exports = {
-    name: 'lewd',
-    description: 'Posts nsfw content uwu',
-    aliases:
-        ['nsfw', 'pn', 'hass', 'hmidriff', 'pgif', '4k', 'hentai', 'holo',
-            'hneko', 'hkitsune', 'anal',
-            'gonewild', 'ass', 'pussy', 'thigh', 'hthigh'],
-    cooldown: 2,
-    flags: flags,
+  name: 'lewd',
+  description: 'Posts nsfw content uwu',
+  aliases: ['nsfw', 'pn', 'hass', 'hmidriff', 'pgif', '4k', 'hentai', 'holo', 'hneko', 'hkitsune', 'anal', 'gonewild', 'ass', 'pussy', 'thigh', 'hthigh'],
+  cooldown: 2,
+  flags: flags,
 
+  execute(message, args) {
+    let search = 'hentai'
 
-    execute(message, args) {
-        let search = 'hentai'
+    const flag = getFlags(flags, args)
+    const al = this.aliases.find(a => message.content.startsWith('.' + a))
 
-        const flag = getFlags(flags, args)
-        const al = this.aliases.find(a => message.content.startsWith('.' + a))
+    let amount = flag.find(f => f.name === 'amount')
+    amount = amount === undefined ? 1 : amount.args
 
-        let amount = flag.find(f => f.name === 'amount')
-        amount = amount === undefined ? 1 : amount.args
-
-        if (amount) {
-            if (amount < 1 || amount > 10) {
-                return message.channel.send('Amount must be between 1 - 10')
-            }
-        }
-
-        if (al && al !== 'nsfw' && al !== 'pn')
-            search = al
-
-        if (message.channel.nsfw === true) {
-            try {
-                for (let i = 0; i < amount; i++) {
-                    agent.get('https://nekobot.xyz/api/image')
-                        .query({ type: search })
-                        .then(result => {
-                            message.channel.send({ file: result.body.message });
-                        }).catch(err => {
-                            console.error(err)
-                        })
-                }
-            }
-            catch (error) {
-            }
-        } else {
-            message.channel.send("This isn't NSFW channel!")
-        }
+    if (amount < 1 || amount > 10) {
+      return message.channel.send('Amount must be between 1 - 10')
     }
+
+    if (al && al !== 'nsfw' && al !== 'pn') search = al
+
+    if (message.channel.nsfw === false) return
+
+    for (let i = 0; i < amount; i++) {
+      agent
+        .get('https://nekobot.xyz/api/image')
+        .query({ type: search })
+        .then(result => {
+          message.channel.send({ file: result.body.message })
+        })
+        .catch(() => console.error)
+    }
+  },
 }
