7d7705e47dac2b880196bf6c1d23b392267d8f66
Kira <lil.kira@outlook.com>
2019-07-12 22:46:58 +0100

    WIP - Music Revising

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,8 +1,12 @@
+const { getBasicInfo } = require('ytdl-core')
+
 module.exports = {
     name: 'play',
     description: 'plays music',
     usage: `[link | search] or [alias]`,
 
-    execute() {
+    AddSong(url) {
+        const song = getBasicInfo(url).then(song).catch(err => console.error(err))
+        console.log(song)
     }
 }
