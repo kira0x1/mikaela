4192a18298c4473fbf050b845b027d210a6246aa
Kira <lil.kira@outlook.com>
2019-07-13 00:03:56 +0100

    updating

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,12 +1,38 @@
 const { getBasicInfo } = require('ytdl-core')
+const youTubeKey = require('../../config.json').keys.youTubeKey
+const search = require('youtube-search')
+
+const opts = {
+    part: ['snippet', 'contentDetails'],
+    maxResults: 5,
+    key: youTubeKey,
+}
+
 
 module.exports = {
     name: 'play',
     description: 'plays music',
     usage: `[link | search] or [alias]`,
+    aliases: ['p', 'play'],
 
-    AddSong(url) {
-        const song = getBasicInfo(url).then(song).catch(err => console.error(err))
-        console.log(song)
-    }
+    async FindSong(query) {
+    },
+
+    //ANCHOR Gets song by url
+    async getSong(url) {
+        let song = await getBasicInfo(url).then(song => song).catch(err => { })
+        return song
+    },
+
+    //Search Function
+    async searchVideo(query) {
+        await search(query, opts)
+            .then(data => {
+                song = data.results[0]
+                addSong(song.link, song.title)
+            })
+            .catch(err => {
+                console.log(`**Couldnt find video:** *${query}*`)
+            })
+    },
 }
--- commands/protoMusic.js
+++ commands/protoMusic.js
@@ -1,5 +1,16 @@
 const fs = require('fs')
 const { findSubCommand } = require('../util/commandUtil')
+const youTubeKey = require('../config.json').keys.youTubeKey
+const search = require('youtube-search')
+const ytdl = require('ytdl-core')
+
+const opts = {
+    part: ['snippet', 'contentDetails'],
+    maxResults: 1,
+    key: youTubeKey,
+}
+
+
 const commandFiles = fs.readdirSync('./commands/music').filter(file => file.endsWith('.js'))
 let subcommands = []
 
@@ -16,10 +27,33 @@ module.exports = {
     subcommands: subcommands,
     guildOnly: true,
 
-    execute(message, args) {
+    async execute(message, args) {
         let cmd = (findSubCommand(message.content.slice(1).split(/ +/)[0]))
-        if (cmd) {
-            console.log(`Subcommand: ${cmd.name} called!`)
+        const song = await findVideoBylink(args.join(' '))
+
+        async function searchVideo(query) {
+            await search(query, opts)
+                .then(data => {
+                    song = data.results[0]
+                    console.log(song.link, song.title)
+                })
+                .catch(() => {
+                    console.log(`**Couldnt find video:** *${query}*`)
+                })
+        }
+
+        async function findVideoBylink(query) {
+            //Check if its a link
+            await ytdl
+                .getBasicInfo(query)
+                .then(song => {
+                    title = song.title
+                    url = song.video_url
+                    console.log(`Found song: ${title}`)
+                })
+                .catch(err => {
+                    searchVideo(query)
+                }) //If not link then search
         }
     }
 }
