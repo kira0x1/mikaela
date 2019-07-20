53db58ac4132f78e004e8f00667a6aac937d7109
Kira <lil.kira@outlook.com>
2019-07-20 08:51:05 +0100

    Cleaned up reddit.js

--- commands/reddit.js
+++ commands/reddit.js
@@ -1,13 +1,8 @@
-const Discord = require('discord.js')
 const fetch = require('node-fetch')
+const { getFlags, getFlagArgs } = require('../util/util')
 
-const { getFlags } = require('../util/util')
-
-const maxpost = 5 //How many images to post
-let searchLimit = 20 //Amount to request from the api
-
-const checkWords = ['i.imgur.com', 'jpg', 'png', 'gif', 'gfycat.com', 'webm',]
-const gyfwords = ['gfycat.com']
+const maxpost = 10 //How many images to post
+let searchLimit = 30 //Amount to request from the api
 
 const flags = [
   (name = { name: 'sort', aliases: ['top', 'hot', 'new', 'controversial', 'rising'] }),
@@ -28,12 +23,11 @@ module.exports = {
   async execute(message, args) {
     const subreddit = args[0]
     let flagsFound = getFlags(flags, args)
-    let sort = 'hot'
-    let time = 'all'
-    let amount = 1
 
     //NOTE Set flags
-    initFlags()
+    let sort = getFlagArgs(flagsFound, 'sort', 'hot')
+    let amount = getFlagArgs(flagsFound, 'amount', 1)
+    let time = getFlagArgs(flagsFound, 'time', 'all')
 
     //NOTE Make sure amount is within range
     if (amount < 1 || amount > maxpost) {
@@ -55,12 +49,6 @@ module.exports = {
       if (!data) return
 
       reply.push(`**${data.title}:** ${data.url}`)
-
-      //NOTE Create embed
-      // const embed = createEmbed(data.title, data.url)
-      //NOTE send message :)
-      // if (embed)
-      // await message.channel.send(embed)
     }
 
     message.channel.send(reply.join('\n'))
@@ -90,60 +78,6 @@ module.exports = {
       }
 
       return postChildren
-    }
-
-    function getImage(url) {
-      const domain = checkWords.find(word => url.includes(word))
-      const isGifCat = gyfwords.find(word => url.includes(word))
-      let urlText = undefined
-
-      if (domain) {
-        urlText = url
-        if (isGifCat) {
-          const gifcat = `http://.*gfycat.com/${url}`
-          urlText = `http://giant.gfycat.com/${gifcat}.gif`
-        }
-      }
-
-      return urlText
-    }
-
-    function createEmbed(title, url) {
-      if (url === 'self') return
-
-      const postImage = getImage(url)
-      if (!postImage) return
-
-      const embed = new Discord.RichEmbed()
-        .setColor(0xc71459)
-        .setTitle(title)
-        .addField(
-          '\nSort:',
-          `
-          ${sort}/${time}`
-        )
-        .setFooter(`From r/${subreddit}`)
-
-      embed.setImage(postImage)
-      return embed
-    }
-
-    function initFlags() {
-      flagsFound.map(fg => {
-        if (fg.args) {
-          switch (fg.name) {
-            case 'sort':
-              sort = fg.args
-              break
-            case 'time':
-              time = fg.args
-              break
-            case 'amount':
-              amount = fg.args
-              break
-          }
-        }
-      })
     }
   },
 }
