6a9f9a696396bca324499b75378771bcd40e596a
lil-kira <lil.kira@outlook.com>
2019-08-27 17:41:32 +0300

    cleaned up init

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { Client } from "discord.js";
 import { token } from "./config";
 import { init as initDb } from "./db/dbSetup";
-import { Init } from "./util/CommandUtil";
+import { Init as cmdInit } from "./util/CommandUtil";
 import { init as emojiInit } from "./util/Emoji";
 import { OnMessage } from "./util/MessageHandler";
 const client = new Client();
 
-client.once("ready", async () => {
+client.once("ready", () => {
   console.log(`${client.user.username} online :3`);
+  cmdInit();
   emojiInit(client);
-  Init();
 });
 
 client.on("message", message => {
