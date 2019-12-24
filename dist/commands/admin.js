5fdd0e6a8a8705671beb38d7253362c457ddf843
lil-kira <lil.kira@outlook.com>
2019-12-24 04:05:40 +0300

    clear out

--- dist/commands/admin.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.command = {
-    name: "admin",
-    execute: function (message, args) { }
-};
--- src/commands/admin.ts
+++
@@ -1,5 +0,0 @@
-import { Command } from "../objects/command";
-export const command: Command = {
-  name: "admin",
-  execute(message, args) {}
-};
