0f8a237b78f04b8a9448ceb06c957b6fa99140f8
lil-kira <lil.kira@outlook.com>
2019-12-25 14:15:31 +0300

    Removed out folder

--- out/commands/info/admin.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CommandInfo_1 = require("../../classes/CommandInfo");
-exports.info = new CommandInfo_1.CommandInfo("Admin", "Admin Commands", ["sys"], []);
