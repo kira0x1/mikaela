71224e4865184456076be77862b08d23288bccb1
Kira <lil.kira@outlook.com>
2020-04-02 02:47:52 +0300

    Fixed list id's , and delete permissions

--- src/commands/admin/delete.ts
+++ src/commands/admin/delete.ts
@@ -1,26 +1,34 @@
-import { ICommand } from '../../classes/Command';
-import chalk from 'chalk';
+import { ICommand } from "../../classes/Command";
+import chalk from "chalk";
 
 export const command: ICommand = {
-    name: 'delete',
-    description: "Deletes messages",
-    usage: "[amount]",
-    aliases: ["d"],
-    perms: ["admin"],
-    hidden: true,
+  name: "delete",
+  description: "Deletes messages",
+  usage: "[amount]",
+  aliases: ["d"],
+  perms: ["admin"],
+  hidden: true,
 
-    async execute(message, args) {
-        let amount = 2
-        args.find(arg => {
-            if (Number(arg)) {
-                amount = Number(arg) + 1
-            }
-        })
-
-        message.channel.bulkDelete(amount).then(deleted => {
-            console.log(chalk.bgMagenta.bold(`Deleted ${deleted.size} messages`))
-        }).catch(err => {
-            console.error(chalk.bgRed.bold(err))
-        })
+  async execute(message, args) {
+    if (message.author.id !== "177016697117474816") {
+      message.reply("You do not have permission to use this command");
+      return;
     }
-}
+
+    let amount = 2;
+    args.find(arg => {
+      if (Number(arg)) {
+        amount = Number(arg) + 1;
+      }
+    });
+
+    message.channel
+      .bulkDelete(amount)
+      .then(deleted => {
+        console.log(chalk.bgMagenta.bold(`Deleted ${deleted.size} messages`));
+      })
+      .catch(err => {
+        console.error(chalk.bgRed.bold(err));
+      });
+  }
+};
