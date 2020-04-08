56639a2eaf850dbd700f90603096cd9ebf90cbf7
Kira <lil.kira@outlook.com>
2020-04-08 05:03:59 +0300

    Changed mikaelas github message

--- src/commands/ungrouped/github.ts
+++ src/commands/ungrouped/github.ts
@@ -3,20 +3,11 @@ import { QuickEmbed, embedColor } from "../../util/Style";
 import { RichEmbed } from "discord.js";
 
 export const command: ICommand = {
-  name: "Github",
-  description: "Links mikaelas github",
-  aliases: ["source", "code"],
+   name: "Github",
+   description: "Links mikaelas github",
+   aliases: ["source", "code"],
 
-  async execute(message, args) {
-    const embed = new RichEmbed()
-      .setTitle("Github")
-      .addField(
-        "URL",
-        `\`\`\`yaml
-https://github.com/lil-kira/MikaelaBot/\`\`\``
-      )
-      .setColor(embedColor);
-
-    message.channel.send(embed);
-  }
+   async execute(message, args) {
+      message.channel.send(`> **Mikaela's Github**\nhttps://github.com/lil-kira/MikaelaBot/`);
+   },
 };
