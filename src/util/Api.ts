7a009657f78493bf7093688ab8399ed3c141c268
Kira <lil.kira@outlook.com>
2020-04-08 05:03:42 +0300

    Songs are now sorted by relevancy

--- src/util/Api.ts
+++ src/util/Api.ts
@@ -1,89 +1,95 @@
-import rp from 'request-promise'
-import { youTubeKey } from '../config'
-import { ConvertDuration } from './musicUtil';
-import { getInfo } from 'ytdl-core';
-import { ISong } from '../classes/Player';
-import ytdl from 'ytdl-core';
+import rp from "request-promise";
+import { youTubeKey } from "../config";
+import { ConvertDuration } from "./musicUtil";
+import { getInfo } from "ytdl-core";
+import { ISong } from "../classes/Player";
+import ytdl from "ytdl-core";
 
 export function Get(url: string, options?: any) {
-    if (!options)
-        options = {
-            method: 'GET',
-            json: true
-        }
+   if (!options)
+      options = {
+         method: "GET",
+         json: true,
+      };
 
-    return new Promise((resolve, reject) => {
-        rp(url, options)
-            .then(body => resolve(body))
-            .catch(err => reject(err))
-    })
+   return new Promise((resolve, reject) => {
+      rp(url, options)
+         .then((body) => resolve(body))
+         .catch((err) => reject(err));
+   });
 }
 
 export function rand(max: number) {
-    return Math.floor(Math.random() * max)
+   return Math.floor(Math.random() * max);
 }
 
 export async function GetSong(query: string): Promise<ISong> {
-    return new Promise((resolve, reject) => {
-        ytdl.getInfo(query).then(info => {
+   return new Promise((resolve, reject) => {
+      ytdl
+         .getInfo(query)
+         .then((info) => {
             let songResult: ISong = {
-                title: info.title,
-                id: info.video_id,
-                url: info.video_url,
-                duration: ConvertDuration(info.length_seconds)
-            }
+               title: info.title,
+               id: info.video_id,
+               url: info.video_url,
+               duration: ConvertDuration(info.length_seconds),
+            };
 
-            return resolve(songResult)
-        }).catch(err => {
-            Youtube.Get(query).then(songResult => {
-                if (songResult)
-                    return resolve(songResult)
-                else {
-                    return reject(undefined)
-                }
-            }).catch(err => {
-                return reject(undefined)
-            })
-        })
-    })
+            return resolve(songResult);
+         })
+         .catch((err) => {
+            Youtube.Get(query)
+               .then((songResult) => {
+                  if (songResult) return resolve(songResult);
+                  else {
+                     return reject(undefined);
+                  }
+               })
+               .catch((err) => {
+                  return reject(undefined);
+               });
+         });
+   });
 }
 
 const options = {
-    part: 'snippet',
-    maxResults: 1,
-    order: 'viewCount',
-    key: youTubeKey,
-}
+   part: "snippet",
+   maxResults: 1,
+   order: "relevance",
+   key: youTubeKey,
+};
 
 export class Youtube {
-    public static async Get(query: string) {
-        const url = `https://www.googleapis.com/youtube/v3/search?key=${options.key}&part=${options.part}&maxResults=${options.maxResults}&order=${options.order}&q=${query}`
+   public static async Get(query: string) {
+      const url = `https://www.googleapis.com/youtube/v3/search?key=${options.key}&part=${options.part}&maxResults=${options.maxResults}&order=${options.order}&q=${query}`;
 
-        let song: ISong | undefined = undefined
-        let id: string | undefined = undefined
+      let song: ISong | undefined = undefined;
+      let id: string | undefined = undefined;
 
-        await rp(url).then(data => {
-            let body: ISongSearch = JSON.parse(data)
-            id = body.items[0].id.videoId
-        }).catch(err => console.log("couldnt find id"))
+      await rp(url)
+         .then((data) => {
+            let body: ISongSearch = JSON.parse(data);
+            id = body.items[0].id.videoId;
+         })
+         .catch((err) => console.log("couldnt find id"));
 
-        if (id) {
-            let res = await getInfo(`https://www.youtube.com/watch?v=${id}`)
-            song = {
-                title: res.title,
-                url: res.video_url,
-                id: id,
-                duration: ConvertDuration(res.length_seconds)
-            }
-        }
+      if (id) {
+         let res = await getInfo(`https://www.youtube.com/watch?v=${id}`);
+         song = {
+            title: res.title,
+            url: res.video_url,
+            id: id,
+            duration: ConvertDuration(res.length_seconds),
+         };
+      }
 
-        return song
-    }
+      return song;
+   }
 }
 
 export interface ISongSearch {
-    items: Array<{
-        id: { videoId: string },
-        snippet: { title: string }
-    }>
+   items: Array<{
+      id: { videoId: string };
+      snippet: { title: string };
+   }>;
 }
