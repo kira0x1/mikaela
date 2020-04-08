4afcd6350c175ca7ad09fe162f33ff4283eb58df
Kira <lil.kira@outlook.com>
2020-04-09 02:28:03 +0300

    Changed github command aliases

--- src/commands/ungrouped/github.ts
+++ src/commands/ungrouped/github.ts
@@ -1,11 +1,9 @@
 import { ICommand } from "../../classes/Command";
-import { QuickEmbed, embedColor } from "../../util/Style";
-import { RichEmbed } from "discord.js";
 
 export const command: ICommand = {
    name: "Github",
    description: "Links mikaelas github",
-   aliases: ["source", "code"],
+   aliases: ["git", "code"],
 
    async execute(message, args) {
       message.channel.send(`> **Mikaela's Github**\nhttps://github.com/lil-kira/MikaelaBot/`);
