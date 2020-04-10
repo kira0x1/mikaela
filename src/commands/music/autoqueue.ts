efe9fc2934d2965536d1eab83d5e8ef4316cbb25
Kira <lil.kira@outlook.com>
2020-04-10 10:13:57 +0300

    ( WIP ) AutoQueue command Skeleton

---
+++ src/commands/music/autoqueue.ts
@@ -0,0 +1,12 @@
+import { ICommand } from "../../classes/Command";
+
+export const command: ICommand = {
+   name: "AutoQueue",
+   description: "Auto Queue's random song's from peoples favorites",
+   aliases: ["aq"],
+   args: false,
+
+   async execute(message, args) {
+       
+   },
+};
