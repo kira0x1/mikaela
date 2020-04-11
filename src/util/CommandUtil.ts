796dc920ec6923fa8f6bc144c04d55a6a2dd2b5a
Kira <lil.kira@outlook.com>
2020-04-12 01:34:17 +0300

    Cleaned up hasPerms function

--- src/util/CommandUtil.ts
+++ src/util/CommandUtil.ts
@@ -10,27 +10,27 @@ export const commands: Collection<string, ICommand> = new Collection();
 export const commandGroups: Collection<string, ICommand[]> = new Collection();
 export const commandInfos: Collection<string, CommandInfo> = new Collection();
 
-export function InitCommands() {
+export function initCommands() {
    const infos: CommandInfo[] = [];
-   readdirSync(path.join(__dirname, '..', 'commands', 'info')).map((file) => {
+   readdirSync(path.join(__dirname, '..', 'commands', 'info')).map(file => {
       const { info } = require(path.join(__dirname, '..', 'commands', 'info', file));
       infos.push(info);
    });
 
    readdirSync(path.join(__dirname, '..', 'commands'))
-      .filter((file) => file.endsWith('js'))
-      .map((file) => {
+      .filter(file => file.endsWith('js'))
+      .map(file => {
          const { command } = require(path.join(__dirname, '..', 'commands', file));
          const cmd: ICommand = command;
          commands.set(cmd.name, cmd);
       });
 
    readdirSync(path.join(__dirname, '..', 'commands'))
-      .filter((folder) => folder !== 'info')
-      .filter((file) => file.endsWith('.js') === false)
-      .map((folder) => {
+      .filter(folder => folder !== 'info')
+      .filter(file => file.endsWith('.js') === false)
+      .map(folder => {
          const folderCommands: ICommand[] = [];
-         readdirSync(path.join(__dirname, '..', 'commands', folder)).map((file) => {
+         readdirSync(path.join(__dirname, '..', 'commands', folder)).map(file => {
             const { command } = require(path.join(__dirname, '..', 'commands', folder, file));
             const cmd: ICommand = command;
             folderCommands.push(cmd);
@@ -44,50 +44,57 @@ export function InitCommands() {
             commandGroups.set(folder, folderCommands);
          }
 
-         const info = infos.find((cmd) => cmd.name.toLowerCase() === folder);
+         const info = infos.find(cmd => cmd.name.toLowerCase() === folder);
          if (info) {
-            const newInfo = new CommandInfo(info.name, info.description, info.aliases, commandGroups.get(info.name.toLowerCase()) || [], info.override);
+            const newInfo = new CommandInfo(
+               info.name,
+               info.description,
+               info.aliases,
+               commandGroups.get(info.name.toLowerCase()) || [],
+               info.override,
+               info.perms
+            );
             commandInfos.set(newInfo.name.toLowerCase(), newInfo);
          }
       });
 }
 
-export function FindCommand(query: string): ICommand | undefined {
+export function findCommand(query: string): ICommand | undefined {
    let command = commands.get(query.toLowerCase());
    if (!command) {
       const cmdArray = commands.array();
-      command = cmdArray.find((cmd) => cmd.aliases && cmd.aliases.find((al) => al.toLowerCase() === query.toLowerCase()));
+      command = cmdArray.find(cmd => cmd.aliases && cmd.aliases.find(al => al.toLowerCase() === query.toLowerCase()));
    }
 
    return command;
 }
 
-export function GetCommandOverride(query: string): ICommand | undefined {
-   const info = commandInfos.find((cmd_info) => cmd_info.name.toLowerCase() === query || cmd_info.aliases.includes(query.toLowerCase()));
+export function getCommandOverride(query: string): ICommand | undefined {
+   const info = commandInfos.find(cmd_info => cmd_info.name.toLowerCase() === query || cmd_info.aliases.includes(query.toLowerCase()));
 
    if (info) {
       if (!info.override) return;
-      const grp = FindCommandGroup(info.name);
-      if (grp) return grp.find((cmd) => cmd.name.toLowerCase() === info.override.toLowerCase());
+      const grp = findCommandGroup(info.name);
+      if (grp) return grp.find(cmd => cmd.name.toLowerCase() === info.override.toLowerCase());
    }
 }
 
-export function FindCommandGroup(query: string) {
+export function findCommandGroup(query: string) {
    let grp = commandGroups.get(query);
    if (!grp) {
-      const info = commandInfos.find((info) => info.name.toLowerCase() === query.toLowerCase() || info.aliases.includes(query.toLowerCase()));
+      const info = commandInfos.find(info => info.name.toLowerCase() === query.toLowerCase() || info.aliases.includes(query.toLowerCase()));
       if (info) grp = info.commands;
    }
    return grp;
 }
 
-export function FindCommandInfo(query: string) {
-   let infoFound = commandInfos.find((info) => info.name.toLowerCase() === query.toLowerCase() || info.aliases.includes(query.toLowerCase()));
+export function findCommandInfo(query: string) {
+   let infoFound = commandInfos.find(info => info.name.toLowerCase() === query.toLowerCase() || info.aliases.includes(query.toLowerCase()));
    return infoFound;
 }
 
-export function GetUsage(query: string) {
-   const command = FindCommand(query);
+export function getUsage(query: string) {
+   const command = findCommand(query);
    if (!command) return console.log(`command: ${query} not found`);
 
    if (command.usage) {
@@ -96,32 +103,12 @@ export function GetUsage(query: string) {
    }
 }
 
-export function HasPerms(userId: string, query: string) {
-   const command = FindCommand(query);
-   let hasPerm = true;
+export function hasPerms(userId: string, query: string): boolean {
+   //Get ID's
+   const permsFound = findCommandInfo(query)?.perms || findCommand(query)?.perms;
 
-   if (command && command.perms) {
-      command.perms.map((permName) => {
-         const perm = perms.find((p) => p.name === permName);
-         if (perm) {
-            hasPerm = perm.users.includes(userId);
-         }
-      });
-   }
+   //If none set then this command does not require permissions to use
+   if (!permsFound || permsFound.length === 0) return true;
 
-   if (!command) {
-      const cmdInfo = commandInfos.get(query);
-      if (cmdInfo) {
-         if (cmdInfo.perms) {
-            cmdInfo.perms.map((permName) => {
-               const perm = perms.find((p) => p.name === permName);
-               if (perm) {
-                  hasPerm = perm.users.includes(userId);
-               }
-            });
-         }
-      }
-   }
-
-   return hasPerm;
+   return permsFound.includes(userId);
 }
