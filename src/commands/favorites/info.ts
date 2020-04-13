95117d274b4434e61d83ed626c1bcb736bc3f144
Kira <lil.kira@outlook.com>
2020-04-13 03:04:22 +0300

    Slightly cleaned up command

--- src/commands/favorites/info.ts
+++ src/commands/favorites/info.ts
@@ -1,26 +1,26 @@
-import { ICommand } from "../../classes/Command";
-import { findFavorite } from "./play";
+import { ICommand } from '../../classes/Command';
+import { findFavorite } from './play';
 import { RichEmbed } from 'discord.js';
 import { embedColor } from '../../util/Style';
 
 export const command: ICommand = {
-    name: "info",
-    description: "Get a songs info",
-    aliases: ["i"],
-    args: true,
-    usage: "",
+   name: 'info',
+   description: 'Get a songs info',
+   aliases: ['i'],
+   args: true,
+   usage: '',
 
-    async execute(message, args) {
-        const song = await findFavorite(message, args)
-        if (!song) return console.log("song not found")
+   async execute(message, args) {
+      const song = await findFavorite(message, args);
+      if (!song || song instanceof Array) return console.log('song not found');
 
-        const embed = new RichEmbed()
-            .setColor(embedColor)
-            .setTitle(song.title)
-            .setDescription(`id: ${song.id}`)
-            .addField("Duration", song.duration.duration)
-            .setURL(song.url)
+      const embed = new RichEmbed()
+         .setColor(embedColor)
+         .setTitle(song.title)
+         .setDescription(`id: ${song.id}`)
+         .addField('Duration', song.duration.duration)
+         .setURL(song.url);
 
-        message.channel.send(embed)
-    }
-}
+      message.channel.send(embed);
+   },
+};
