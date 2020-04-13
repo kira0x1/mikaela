98af2d120b9492a751703055d7d5789d8ef55b94
Kira <lil.kira@outlook.com>
2020-04-13 03:56:06 +0300

    Cleaned up add command

--- src/commands/music/add.ts
+++ src/commands/music/add.ts
@@ -1,28 +1,23 @@
 import { ICommand } from '../../classes/Command';
 import { getPlayer } from '../../app';
-import { GetSong } from '../../util/Api';
+import { getSong } from '../../util/Api';
 import { QuickEmbed } from '../../util/Style';
 
 export const command: ICommand = {
-   name: 'Add',
-   description: 'Add song to queue',
+  name: 'Add',
+  description: 'Add song to queue',
 
-   async execute(message, args) {
-      //Get the guilds player
-      const player = getPlayer(message);
+  async execute(message, args) {
+    //Get the guilds player
+    const player = getPlayer(message);
 
-      if (player) {
-         const query = args.join(' ');
+    if (player) {
+      const query = args.join(' ');
 
-         //Get song
-         GetSong(query)
-            .then((song) => {
-               //If the song is found add it to the queue
-               player.queue.addSong(song);
-            })
-            .catch((err) => {
-               QuickEmbed(message, 'Song not found');
-            });
-      }
-   },
+      //Get song
+      getSong(query)
+        .then(song => player.queue.addSong(song)) //? If the song is found add it to the queue
+        .catch(err => QuickEmbed(message, 'Song not found'));
+    }
+  },
 };
