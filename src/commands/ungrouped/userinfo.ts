b2e870c20865a15dd8321e0246424dc1cb5f8e41
BIRD UP <birdupthemusical@outlook.com>
2020-07-30 22:43:28 +0200

    Handle error in userinfo, move sending embed to separate function

--- src/commands/ungrouped/userinfo.ts
+++ src/commands/ungrouped/userinfo.ts
@@ -1,7 +1,7 @@
 import { ICommand } from '../../classes/Command';
-import { User } from 'discord.js';
+import { Message, User } from 'discord.js';
 import { parseUser } from '../../util/parser';
-import { createFooter } from '../../util/Style';
+import { createFooter, sendErrorEmbed } from '../../util/Style';
 
 export const command: ICommand = {
     name: 'userinfo',
@@ -9,24 +9,38 @@ export const command: ICommand = {
     aliases: ['av'],
 
     async execute(message, args) {
-        let user: User = (await parseUser(args[0], message.client)) || message.author;
+        let user: User = null;
 
-        const embed = createFooter(message.client);
+        if (args.length == 0) {
+            user = message.author;
+        } else {
+            user = await parseUser(args[0], message.client);
 
-        embed.setTitle('User info');
-        embed.setDescription(`User info for ${user}`);
-
-        embed.setThumbnail(user.avatarURL());
-
-        embed.addField('User ID', `\`${user.id}\``);
-        embed.addField('Created at', user.createdAt.toUTCString());
-
-        let member = message.guild.member(user);
-
-        if (member) {
-            embed.addField('Joined at', member.joinedAt.toUTCString());
+            if (!user) {
+                return await sendErrorEmbed(message, `Cannot find user ${args[0]}`);
+            }
         }
 
-        await message.channel.send(embed);
+        await sendEmbed(message, user);
     },
 };
+
+async function sendEmbed(message: Message, user: User) {
+    const embed = createFooter(message.client);
+
+    embed.setTitle('User info');
+    embed.setDescription(`User info for ${user}`);
+
+    embed.setThumbnail(user.avatarURL());
+
+    embed.addField('User ID', `\`${user.id}\``);
+    embed.addField('Created at', user.createdAt.toUTCString());
+
+    let member = message.guild.member(user);
+
+    if (member) {
+        embed.addField('Joined at', member.joinedAt.toUTCString());
+    }
+
+    await message.channel.send(embed);
+}
