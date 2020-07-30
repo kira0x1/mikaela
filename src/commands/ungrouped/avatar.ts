8b7279dd843269010d2869c3f7e8378b36ea4364
BIRD UP <birdupthemusical@outlook.com>
2020-07-30 22:29:00 +0200

    test push

--- src/commands/ungrouped/avatar.ts
+++ src/commands/ungrouped/avatar.ts
@@ -19,6 +19,7 @@ export const command: ICommand = {
         embed.setTitle('Avatar');
         embed.setDescription(`Avatar of ${user}`);
 
+
         embed.setImage(user.avatarURL({'size': 4096}))
 
         embed.setTimestamp(new Date());
