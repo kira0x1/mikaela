9e5887df9e3a0fbc9b8a41a297b8d9077fba27e0
BIRD UP <birdupthemusical@outlook.com>
2020-07-30 21:51:54 +0200

    Refactor

--- src/commands/ungrouped/roleinfo.ts
+++ src/commands/ungrouped/roleinfo.ts
@@ -1,6 +1,6 @@
 import { ICommand } from '../../classes/Command';
 import { createFooter, sendErrorEmbed } from '../../util/Style';
-import { Guild, MessageEmbed, Role } from 'discord.js';
+import { Guild, Message, MessageEmbed, Role } from 'discord.js';
 
 export const command: ICommand = {
     name: 'roleinfo',
@@ -9,28 +9,35 @@ export const command: ICommand = {
     args: true,
 
     async execute(message, args) {
-        let embed: MessageEmbed = createFooter(message.client);
-        let role: Role = await parseRole(args[0], message.guild);
+        let arg: string = args[0];
+        let role: Role = await parseRole(arg, message.guild);
 
         if (!role) {
-            return await sendErrorEmbed(message, `Cannot find role ${role}`);
+            await sendErrorEmbed(message, `Cannot find role ${role}`);
+            return;
         }
 
-        embed.setTitle('Role info');
-        embed.setDescription(`Role info for ${role}`);
-
-        embed.setColor(role.color);
-
-        embed.addField('Role ID', `\`${role.id}\``);
-        embed.addField('Created at', role.createdAt.toUTCString());
-        embed.addField('Hoist', role.hoist, true);
-        embed.addField('Mentionable', role.mentionable, true);
-        embed.addField('Position', role.position, true);
-        embed.addField('Members with role', role.members.size, true);
-
-        await message.channel.send(embed);
+        await sendEmbed(message, role);
     },
 };
+
+async function sendEmbed(message: Message, role: Role) {
+    let embed: MessageEmbed = createFooter(message.client);
+
+    embed.setTitle('Role info');
+    embed.setDescription(`Role info for ${role}`);
+
+    embed.setColor(role.color);
+
+    embed.addField('Role ID', `\`${role.id}\``);
+    embed.addField('Created at', role.createdAt.toUTCString());
+    embed.addField('Hoist', role.hoist, true);
+    embed.addField('Mentionable', role.mentionable, true);
+    embed.addField('Position', role.position, true);
+    embed.addField('Members with role', role.members.size, true);
+
+    await message.channel.send(embed);
+}
 
 const ROLE_MENTION_PATTERN = /<@&|>/g;
 
