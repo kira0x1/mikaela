53c1df41781935403ba31bd67adf27d135b0a3b3
Kira <kira.eva@protonmail.com>
2020-10-07 23:15:52 +0300

    New move command, to move songs around the queue

---
+++ src/commands/music/move.ts
@@ -0,0 +1,44 @@
+import { MessageEmbed } from 'discord.js';
+import { ICommand } from '../../classes/Command';
+import { getPlayer } from '../../util/musicUtil';
+import { embedColor } from '../../util/styleUtil';
+
+export const command: ICommand = {
+    name: 'Move',
+    description: 'Move a commands position in the Queue',
+    args: true,
+    usage: '[song position] [desired position]',
+
+    execute(message, args) {
+        console.log(args)
+
+        const player = getPlayer(message)
+        if (player.getQueueCount() === 0) return message.channel.send('Queue is empty')
+
+        if (args.length < 2) return message.channel.send('Not enough arguments..')
+
+        let songPos: string | number = args.shift();
+        let toPos: string | number = args.shift();
+
+        songPos = Number(songPos)
+        toPos = Number(toPos)
+
+        songPos--;
+        toPos--;
+
+        const songSelected = player.queue.songs[songPos]
+        const otherSong = player.queue.songs[toPos]
+
+        if (!songSelected || !otherSong)
+            return message.channel.send('One of the songs selected is undefined')
+
+        player.queue.songs[toPos] = songSelected
+        player.queue.songs[songPos] = otherSong
+
+        const embed = new MessageEmbed().setColor(embedColor)
+            .setTitle(`Moved song from ${songPos} to ${toPos}`)
+            .setAuthor(message.author.username, message.author.avatarURL())
+
+        message.channel.send(embed)
+    }
+}
