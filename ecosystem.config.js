08c07b25cdf4654652bd94585eb6e0dcba9e1ff6
Kira <kira.eva@protonmail.com>
2020-10-13 22:49:40 +0300

    pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [{
+        name: 'mikaela',
+        script: './out/app.js',
+        exp_backoff_restart_delay: 100,
+        args: ['--color', '--prod-db'],
+        error_file: './logs/err.log',
+        out_file: './logs/out.log',
+        log_file: './logs/combined.log',
+        time: true,
+        env: {
+            NODE_ENV: 'development',
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            args: ['--production', '--color', '--logcommands'],
+        },
+    }, ],
+};
