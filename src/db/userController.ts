bc386f955abd31d9744d409292ed4549f91e2871
Kira <kira.eva@protonmail.com>
2020-10-13 07:16:15 +0300

    Made getting user from mongo more efficient.
    Using the lean() function from mongoose now.
    Also cleaned up async promise

--- src/db/userController.ts
+++ src/db/userController.ts
@@ -1,25 +1,25 @@
 import { conn } from './database';
-import { IUser, userModel as User, UserSchema } from './dbUser';
+import { IUser, UserSchema } from './dbUser';
 
 // - GET - /users # returns all users
-export let allUsers = () => {
-    return new Promise(function (resolve, reject) {
-        let users = User.find((err: any, users: any) => {
-            if (err) reject(err);
-            else resolve(users);
-        });
-    });
-};
+// export let allUsers = () => {
+//     return new Promise(function (resolve, reject) {
+//         let users = User.find((err: any, users: any) => {
+//             if (err) reject(err);
+//             else resolve(users);
+//         });
+//     });
+// };
 
 // - GET - /user/{1} # returns user with id 1
 export async function getUser(id: string): Promise<IUser> {
-    return new Promise(async function (resolve, reject) {
+    try {
         var userModel = await conn.model('users', UserSchema);
-        await userModel.findOne({ id: id }, (err: any, user: any) => {
-            if (err || user === null) reject(err);
-            else resolve(user);
-        });
-    });
+        const user: any = await userModel.findOne({ id: id })?.lean();
+        return user
+    } catch (err) {
+        return err;
+    }
 }
 
 // - PUT - /user # inserts a new user into the table
