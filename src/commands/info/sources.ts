7751682cf256cbc78154f51f597f84ef2b825f8f
Kira <kira.eva@protonmail.com>
2020-10-13 16:27:39 +0300

    Removed sources commands, needs more work... and much more cleaning up

--- src/commands/info/sources.ts
+++
@@ -1,9 +0,0 @@
-import { CommandInfo } from '../../classes/CommandInfo';
-
-export const info: CommandInfo = new CommandInfo(
-    'Sources',
-    'Organize and store sources',
-    ['source', 'src'],
-    [],
-    'list'
-);
