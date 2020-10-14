60524897ccc2e42182e265324a18e9f4b382a250
Kira <kira.eva@protonmail.com>
2020-10-14 21:27:18 +0300

    Create based.ts

---
+++ src/commands/fun/based.ts
@@ -0,0 +1,13 @@
+import { ICommand } from '../../classes/Command';
+
+const videoUrl = 'https://cdn.discordapp.com/attachments/441778432767164427/765875762657886218/HELLO_BASED_DEPARTMENT.mp4'
+
+export const command: ICommand = {
+    name: 'Based',
+    aliases: ['destiny'],
+    description: 'Hello is this the based department ???? ',
+
+    execute(message, args) {
+        message.channel.send(videoUrl);
+    },
+};
