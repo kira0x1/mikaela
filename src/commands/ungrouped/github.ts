dc5ecee8a14ca87afdad0e526ceefadbed6621f9
Kira <kira.eva@protonmail.com>
2020-10-14 21:59:04 +0300

    Update github.ts

--- src/commands/ungrouped/github.ts
+++ src/commands/ungrouped/github.ts
@@ -1,11 +1,13 @@
 import { ICommand } from '../../classes/Command';
 
+const githubLink = 'https://github.com/kira0x1/mikaela'
+
 export const command: ICommand = {
     name: 'Github',
     description: 'Links mikaelas github',
     aliases: ['git', 'code'],
 
     async execute(message, args) {
-        message.channel.send(`> **Mikaela's Github**\nhttps://github.com/kira0x1/MikaelaBot`);
+        message.channel.send(`> **Mikaela's Github**\n${githubLink}`);
     },
 };
