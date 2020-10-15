f8cf8d876920643f3ae110761b300175b49c3eb9
Kira <kira.eva@protonmail.com>
2020-10-15 08:15:54 +0300

    Created reload command - todo -

---
+++ src/commands/admin/reload.ts
@@ -0,0 +1,10 @@
+import { ICommand } from '../../classes/Command';
+
+export const command: ICommand = {
+    name: "reload",
+    description: "Reloads a command",
+
+    execute(message, args) {
+
+    }
+}
