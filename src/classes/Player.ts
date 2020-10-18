becf28fb491c4f281cfed9de78b5aaa59a9ea156
Kira <kira.eva@protonmail.com>
2020-10-18 20:06:49 +0300

    Loweing highwatermark slightly

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -121,9 +121,9 @@ export class Player {
             return console.error('Could not connect to the voice channel');
          }
 
-         const dispatcher = connection.play(await ytdl(song.url, { filter: 'audioonly', highWaterMark: 1 << 28 }), {
+         const dispatcher = connection.play(await ytdl(song.url, { filter: 'audioonly', highWaterMark: 1 << 26 }), {
             type: 'opus',
-            highWaterMark: 1 << 18,
+            highWaterMark: 1 << 16,
             volume: false
          })
 
