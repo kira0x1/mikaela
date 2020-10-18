c19dc298e7e342b5394d8b37c25cee85dc6aa3cb
Kira <kira.eva@protonmail.com>
2020-10-18 20:04:01 +0300

    Revert "Increased Highwatermark"
    This reverts commit 25bf99f16401c28ee52e27dbb8294719f8b0dc5a.

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -121,9 +121,9 @@ export class Player {
             return console.error('Could not connect to the voice channel');
          }
 
-         const dispatcher = connection.play(await ytdl(song.url, { filter: 'audioonly', highWaterMark: 1 << 28 }), {
+         const dispatcher = connection.play(await ytdl(song.url, { filter: 'audioonly', highWaterMark: 1 << 25 }), {
             type: 'opus',
-            highWaterMark: 1 << 20,
+            highWaterMark: 1 << 15,
             volume: false
          })
 
