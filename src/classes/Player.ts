999cfa30dc776865232f0ac599f089050c737e70
Kira <kira.eva@protonmail.com>
2020-10-19 10:03:53 +0300

    highwater mark tweak

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -122,10 +122,10 @@ export class Player {
             return console.error('Could not connect to the voice channel');
          }
 
-         const dispatcher = connection.play(await ytdl(song.url, { filter: 'audioonly', quality: 'highestaudio' }), {
+         const dispatcher = connection.play(await ytdl(song.url, { filter: 'audioonly', quality: 'highestaudio', 1<< 25 }), {
             type: 'opus',
             volume: false,
-            highWaterMark: 1 << 25
+            highWaterMark: 1
          })
 
          //! Check if volumeDisabled is false, if it is then set the volume
