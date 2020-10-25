348b26e9371a0c0047d3b059529bf447bc0c42df
Kira <kira.eva@protonmail.com>
2020-10-25 13:38:49 +0300

    New command "Yubi"

---
+++ src/commands/fun/yubi.ts
@@ -0,0 +1,14 @@
+import { ICommand } from '../../classes/Command';
+
+const yubiLink =
+    'https://cdn.discordapp.com/attachments/644126670080573460/764113835291705384/korone_yaaay.webm';
+
+export const command: ICommand = {
+    name: 'yubi',
+    description: 'YUBI YUBI!!! 🐱‍💻',
+    aliases: ['yubiyubi', 'finger', 'yaay', 'yay'],
+
+    execute(message, args: string[]) {
+        message.channel.send(yubiLink);
+    },
+};
