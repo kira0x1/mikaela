114b5e918072862a95636b1403425027fc0bbf56
Kira <kira.eva@protonmail.com>
2020-10-26 06:15:50 +0300

    New command searching

---
+++ src/commands/music/search.ts
@@ -0,0 +1,39 @@
+import { ICommand } from '../../classes/Command';
+import { convertPlaylistToSongs, getSong, isPlaylist } from '../../util/apiUtil';
+import { createFooter } from '../../util/styleUtil';
+
+export const command: ICommand = {
+    name: 'search',
+    description: 'Search for a song',
+    args: true,
+
+    async execute(message, args) {
+        const query = args.join(' ')
+        const song = await getSong(query)
+        const embed = createFooter(message)
+
+        if (!song) {
+            embed.setTitle(`Song "${query}" not found`)
+            return message.channel.send(embed)
+        }
+
+
+        if (isPlaylist(song)) {
+            embed.setTitle(song.title)
+                .setDescription(`${song.total_items} Songs\n`)
+
+            const songs = await convertPlaylistToSongs(song)
+
+            for (let i = 0; i < songs.length - 1 && i < 20; i++) {
+                embed.addField(`${i + 1} ${songs[i].title}`, songs[i].url);
+            }
+
+            message.channel.send(embed)
+            return
+        }
+
+
+        embed.setTitle(song.title).setDescription(song.duration.duration)
+        message.channel.send(embed)
+    }
+}
