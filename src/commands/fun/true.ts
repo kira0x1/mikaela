caa7c071b1a74f3bf93e68a2c41dc5ff90d3211b
Kira <kira.eva@protonmail.com>
2020-11-15 14:36:30 +0300

    New Command

---
+++ src/commands/fun/true.ts
@@ -0,0 +1,13 @@
+import { ICommand } from '../../classes/Command';
+
+const videoUrl = 'https://cdn.discordapp.com/attachments/642037173729624104/777496871001784350/TRUEoooooo.mp4'
+
+export const command: ICommand = {
+    name: 'True',
+    aliases: ['truee', 'real', 'circlejerk'],
+    description: 'Trrrueeeeeeeeeeee!!',
+
+    execute(message, args) {
+        message.channel.send(videoUrl);
+    },
+};
