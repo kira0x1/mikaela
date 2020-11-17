b2b1b3cc3afbc434206e30065a8ec9867efcde29
Kira <kira.eva@protonmail.com>
2020-11-17 10:57:04 +0300

    Increased global volume slightly

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -9,7 +9,7 @@ const maxVolume: number = 10;
 export class Player {
    guild: Guild;
    queue: Queue;
-   volume: number = 0.7;
+   volume: number = 0.9;
    isPlaying: boolean = false;
    inVoice: boolean = false;
    stream: StreamDispatcher | undefined;
