402a1b16fd0a9f5def62650efed0ea5b9fd7e3fc
Kira <kira.eva@protonmail.com>
2020-11-26 00:46:42 +0300

    Added new alias

--- src/commands/server/userinfo.ts
+++ src/commands/server/userinfo.ts
@@ -3,25 +3,27 @@ import { getTargetMember } from '../../util/musicUtil';
 import { createFooter, QuickEmbed } from '../../util/styleUtil';
 
 export const command: ICommand = {
-    name: 'userinfo',
-    description: 'Shows information of a user',
-    aliases: ['info', 'user'],
+   name: 'userinfo',
+   description: 'Shows information of a user',
+   aliases: ['info', 'user', 'uinfo'],
 
-    async execute(message, args) {
-        const target = args.length > 0 ?
-            await getTargetMember(message, args.join(' ')) :
-            message.member;
+   async execute(message, args) {
+      const target =
+         args.length > 0
+            ? await getTargetMember(message, args.join(' '))
+            : message.member;
 
-        if (!target) return QuickEmbed(message, `Could not find user \`${args.join(' ')}\``)
+      if (!target)
+         return QuickEmbed(message, `Could not find user \`${args.join(' ')}\``);
 
-        const embed = createFooter(message)
-            .setTitle('User info')
-            .setDescription(`User info for ${target}`)
-            .setThumbnail(target.user.avatarURL({ dynamic: true, size: 4096 }))
-            .addField('User ID', `\`${target.id}\``)
-            .addField('Created at', target.user.createdAt.toUTCString())
-            .addField('Joined at', target.joinedAt.toUTCString());
+      const embed = createFooter(message)
+         .setTitle('User info')
+         .setDescription(`User info for ${target}`)
+         .setThumbnail(target.user.avatarURL({ dynamic: true, size: 4096 }))
+         .addField('User ID', `\`${target.id}\``)
+         .addField('Created at', target.user.createdAt.toUTCString())
+         .addField('Joined at', target.joinedAt.toUTCString());
 
-        message.channel.send(embed);
-    }
+      message.channel.send(embed);
+   }
 };
