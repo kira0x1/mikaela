3add4459f4a52ef2e01aa30801a29b1e48d2a40d
Kira <kira.eva@protonmail.com>
2020-11-28 17:16:27 +0300

    Removed old commented out code

--- src/db/userController.ts
+++ src/db/userController.ts
@@ -1,16 +1,6 @@
 import { conn } from './database';
 import { IUser, UserSchema } from './dbUser';
 
-// - GET - /users # returns all users
-// export let allUsers = () => {
-//     return new Promise(function (resolve, reject) {
-//         let users = User.find((err: any, users: any) => {
-//             if (err) reject(err);
-//             else resolve(users);
-//         });
-//     });
-// };
-
 // - GET - /user/{1} # returns user with id 1
 export async function getUser(id: string): Promise<IUser> {
     try {
