61007a366a4e267bcb0ab88a6d11497afd83105b
Kira <kira.eva@protonmail.com>
2020-12-04 20:32:09 +0300

    Dont log the title anymore
    also formatted via prettier

--- src/commands/ungrouped/vote.ts
+++ src/commands/ungrouped/vote.ts
@@ -4,58 +4,59 @@ import { ICommand } from '../../classes/Command';
 import { embedColor, QuickEmbed } from '../../util/styleUtil';
 
 const voteEmojis = [
-    { name: 'one' },
-    { name: 'two' },
-    { name: 'three' },
-    { name: 'four' },
-    { name: 'five' },
-    { name: 'six' },
-    { name: 'seven' },
-    { name: 'eight' },
-    { name: 'nine' },
+   { name: 'one' },
+   { name: 'two' },
+   { name: 'three' },
+   { name: 'four' },
+   { name: 'five' },
+   { name: 'six' },
+   { name: 'seven' },
+   { name: 'eight' },
+   { name: 'nine' }
 ];
 
 export const command: ICommand = {
-    name: 'Vote',
-    description: 'Create a strawpoll',
-    usage: '[option1, option2, ...etc] [optional: -title]',
-    aliases: ['strawpoll'],
+   name: 'Vote',
+   description: 'Create a strawpoll',
+   usage: '[option1, option2, ...etc] [optional: -title]',
+   aliases: ['strawpoll'],
 
-    async execute(message, args) {
-        let title = 'strawpoll';
-        let options = args.join(' ').split(',');
+   async execute(message, args) {
+      let title = 'strawpoll';
+      let options = args.join(' ').split(',');
 
-        let votes: string[] = [];
+      let votes: string[] = [];
 
-        //find -title
-        options.map((arg, pos) => {
-            if (arg.includes('-title')) {
-                title = arg.slice(7, arg.length);
-                console.log(title);
-            } else {
-                votes.push(arg);
-            }
-        });
+      //find -title
+      options.map((arg, pos) => {
+         if (arg.includes('-title')) {
+            title = arg.slice(7, arg.length);
+         } else {
+            votes.push(arg);
+         }
+      });
 
-        const embed = new MessageEmbed().setTitle(title).setColor(embedColor);
+      const embed = new MessageEmbed().setTitle(title).setColor(embedColor);
 
-        if (votes.length > 9) {
-            return QuickEmbed(message, `max options 9`);
-        }
+      if (votes.length > 9) {
+         return QuickEmbed(message, `max options 9`);
+      }
 
-        for (let i = 0; i < votes.length; i++) {
-            let vote = votes[i];
-            embed.addField(vote, `${i + 1}`, true);
-        }
+      for (let i = 0; i < votes.length; i++) {
+         let vote = votes[i];
+         embed.addField(vote, `${i + 1}`, true);
+      }
 
-        try {
-            const messageSent = await message.channel.send(embed)
-            for (let i = 0; i < votes.length; i++) {
-                const emoji = messageSent.client.emojis.cache.find(emoji => emoji.name === voteEmojis[i].name);
-                await messageSent.react(emoji.id);
-            }
-        } catch (err) {
-            console.error(err)
-        }
-    },
+      try {
+         const messageSent = await message.channel.send(embed);
+         for (let i = 0; i < votes.length; i++) {
+            const emoji = messageSent.client.emojis.cache.find(
+               emoji => emoji.name === voteEmojis[i].name
+            );
+            await messageSent.react(emoji.id);
+         }
+      } catch (err) {
+         console.error(err);
+      }
+   }
 };
