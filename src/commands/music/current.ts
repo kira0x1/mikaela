b2ebf71326362ee47eccb2cf5a4d33175423b311
Kira <kira.eva@protonmail.com>
2020-12-17 19:11:51 +0300

    Cleaned up command

--- src/commands/music/current.ts
+++ src/commands/music/current.ts
@@ -1,10 +1,10 @@
-import { MessageEmbed, MessageReaction, User } from 'discord.js';
+import { MessageReaction, User } from 'discord.js';
 import ms from 'ms';
 import { ICommand } from '../../classes/Command';
 import { CreateUser, IUser } from '../../db/dbUser';
 import { getUser } from '../../db/userController';
-import { getPlayer } from '../../util/musicUtil';
-import { embedColor, QuickEmbed } from '../../util/styleUtil';
+import { createCurrentlyPlayingEmbed, getPlayer } from '../../util/musicUtil';
+import { QuickEmbed } from '../../util/styleUtil';
 import { AddFavorite } from '../favorites/add';
 import { heartEmoji } from './play';
 
@@ -24,22 +24,8 @@ export const command: ICommand = {
 
         if (!(stream && player.currentlyPlaying)) return QuickEmbed(message, 'No song currently playing');
 
-        const streamTime = (stream.streamTime - stream.pausedTime) / 1000;
-        const minutes = Math.floor(streamTime / 60);
-
-        let seconds: number | string = streamTime - minutes * 60;
-        seconds = seconds < 10 ? '0' + seconds.toFixed(0) : seconds.toFixed(0);
-
-        const duration = player.currentlyPlaying.duration;
-
-        let prettyTime = minutes.toFixed(0) + ':' + seconds;
-
         //Create embed
-        const embed = new MessageEmbed()
-            .setColor(embedColor)
-            .setTitle('Playing: ' + currentSong.title)
-            .setURL(currentSong.url)
-            .addField(`Duration`, `${prettyTime} / ${duration.duration}`);
+        const embed = createCurrentlyPlayingEmbed(stream, player)
 
         const msg = await message.channel.send(embed);
         await msg.react(heartEmoji.id);
