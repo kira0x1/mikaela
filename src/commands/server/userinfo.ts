a813b7d55deb95e965a1655750cde8271a620736
BIRD-UP73 <birdupthemusical@outlook.com>
2021-01-02 18:57:02 +0100

    Add possibility to show previous nicknames for user

--- src/commands/server/userinfo.ts
+++ src/commands/server/userinfo.ts
@@ -1,6 +1,7 @@
 import { ICommand } from '../../classes/Command';
 import { getTargetMember } from '../../util/musicUtil';
 import { createFooter, QuickEmbed } from '../../util/styleUtil';
+import { GuildMember } from 'discord.js';
 
 export const command: ICommand = {
    name: 'userinfo',
@@ -24,6 +25,30 @@ export const command: ICommand = {
          .addField('Created at', target.user.createdAt.toUTCString())
          .addField('Joined at', target.joinedAt.toUTCString());
 
+      if (
+         message.guild.me.hasPermission('VIEW_AUDIT_LOG') &&
+         message.member.hasPermission('VIEW_AUDIT_LOG')
+      ) {
+         const previousNicks = await getPreviousNicks(target);
+
+         if (previousNicks.length > 0) {
+            embed.addField('Previous nicknames', previousNicks.join(', '));
+         }
+      }
+
       message.channel.send(embed);
    }
 };
+
+async function getPreviousNicks(member: GuildMember): Promise<string[]> {
+   return await member.guild
+      .fetchAuditLogs({
+         type: 'MEMBER_UPDATE'
+      })
+      .then(audit =>
+         audit.entries
+            .filter(e => e.target == member.user && e.changes.some(e => e.key == 'nick'))
+            .map(e => e.changes)
+            .flatMap(e => [e[0].old, e[0].new].filter(e => e !== undefined))
+      );
+}
