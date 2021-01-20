3c3385f52a5f7344231d2a5c92de73ca9a885d3c
kira0x1 <kira.eva@protonmail.com>
2021-01-20 16:28:17 +0300

    made the players embed look nicer, and changed the perms

--- src/commands/admin/players.ts
+++ src/commands/admin/players.ts
@@ -1,4 +1,6 @@
+import { Collection, MessageEmbed } from "discord.js";
 import { ICommand } from "../../classes/Command";
+import { Player } from "../../classes/Player";
 import { players } from "../../util/musicUtil";
 import { createFooter } from "../../util/styleUtil";
 
@@ -6,12 +8,18 @@ export const command: ICommand = {
     name: 'Players',
     description: 'List all players',
     aliases: ['streams'],
-    perms: ['kira'],
+    perms: ['admin'],
 
     execute(message, args) {
         const embed = createFooter(message).setTitle(`Player: ${players.size}`)
-        players.map(p => embed.addField(p.guild.name, `IsPlaying: ${p.isPlaying}`))
+
+        createPlayersEmbed(embed, players.filter(p => p.isPlaying))
+        createPlayersEmbed(embed, players.filter(p => !p.isPlaying))
 
         message.channel.send(embed)
     }
+}
+
+function createPlayersEmbed(embed: MessageEmbed, players: Collection<string, Player>) {
+    players.map(p => embed.addField(p.guild.name, `Playing: **${p.isPlaying}**`))
 }
