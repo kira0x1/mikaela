7f8f97352a1c6928ad15e25da64815a892d051f7
kira0x1 <kira.eva@protonmail.com>
2021-03-14 15:55:55 +0300

    added new alias to berserk command

--- src/commands/fun/berserk.ts
+++ src/commands/fun/berserk.ts
@@ -5,7 +5,7 @@ const videoLink = "https://cdn.discordapp.com/attachments/702091543514710027/820
 export const command: ICommand = {
     name: 'Berserk',
     description: 'Posts a video of dosha listening to some 😡 music',
-    aliases: ['epic'],
+    aliases: ['epic', 'dosha'],
 
     async execute(message, args) {
         message.channel.send(videoLink)
