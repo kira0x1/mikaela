2cdf6619dd955140be48df93450f7bcda64f1805
kira0x1 <kira.eva@protonmail.com>
2021-04-02 12:43:51 +0300

    volume permission changes
    - can print current volume without permissions
    - still need mod perms to change volume

--- src/commands/music/volume.ts
+++ src/commands/music/volume.ts
@@ -1,7 +1,8 @@
 import { getPlayer } from '../../util/musicUtil';
 import { ICommand } from '../../classes/Command';
-import { QuickEmbed } from '../../util/styleUtil';
+import { QuickEmbed, wrap } from '../../util/styleUtil';
 import { logger } from '../../app';
+import { hasPerms } from '../../util/commandUtil';
 
 export const command: ICommand = {
    name: 'volume',
@@ -9,16 +10,18 @@ export const command: ICommand = {
    aliases: ['v'],
    usage: '[- | + | number]\n\nDisplays the volume if no arguments given',
    hidden: true,
-   perms: ['mod'],
    isDisabled: false,
 
    execute(message, args) {
       const arg = args.shift();
       const player = getPlayer(message);
-      if (!player)
-         return logger.log('info', 'no player found while using volume command');
 
+      if (!player) return logger.log('info', 'no player found while using volume command');
       if (!arg) return QuickEmbed(message, `Volume is currently ${player.volume}`);
+
+      if (!hasPerms(message.member, this.name)) {
+         return message.author.send(`You do not have permission to use ${wrap(command.name)}`);
+      }
 
       let amount: number | undefined;
 
