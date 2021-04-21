2b61d1f51b01b220d2c1b79ea50983fb7d5344d1
kira0x1 <kira.eva@protonmail.com>
2021-04-21 03:37:18 +0300

    added block command

---
+++ src/commands/fun/block.ts
@@ -0,0 +1,13 @@
+import { ICommand } from '../../classes/Command';
+
+const videoLink = "https://cdn.discordapp.com/attachments/702091543514710027/834224349417832489/video_2.mp4"
+
+export const command: ICommand = {
+    name: 'Blocked',
+    description: 'Posts a video expressing your desire to block someone',
+    aliases: ['block'],
+    args: false,
+    execute(message, args) {
+        message.channel.send(videoLink)
+    }
+}
