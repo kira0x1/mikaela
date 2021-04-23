4a8acc8f674efad5899eb3cba68137b900ca295d
kira0x1 <kira.eva@protonmail.com>
2021-04-23 07:02:12 +0300

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [{
+        name: 'mikaela',
+        script: './out/app.js',
+        args: ['--color'],
+        shutdown_with_message: true,
+        kill_timeout: 15000,
+        error_file: './logs/err.log',
+        out_file: './logs/out.log',
+        log_file: './logs/combined.log',
+        time: true,
+        env: {
+            NODE_ENV: 'development',
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            args: ['--production', '--color'],
+        },
+    }]
+};
