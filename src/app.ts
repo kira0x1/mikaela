354829c3a358a31d00138f57625bbfc6e7cc35bc
kira0x1 <kira.eva@protonmail.com>
2021-04-23 03:16:57 +0300

    removed commented out code

--- src/app.ts
+++ src/app.ts
@@ -43,9 +43,6 @@ const client = new Client({
          url: 'https://github.com/kira0x1/mikaela'
       }
    },
-   // messageCacheMaxSize: 50,
-   // messageCacheLifetime: 60,
-   // messageSweepInterval: 120,
    disableMentions: 'everyone'
 });
 
