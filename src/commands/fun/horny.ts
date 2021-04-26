b4b76143995cb7cb60a847e27531cf2ce6a1d980
kira0x1 <kira.eva@protonmail.com>
2021-04-26 14:06:03 +0300

    Reduced Horny commands options

--- src/commands/fun/horny.ts
+++ src/commands/fun/horny.ts
@@ -2,10 +2,8 @@ import { Command } from '../../classes/Command';
 
 const dogeHorny = 'https://cdn.discordapp.com/attachments/285778400956645376/752686497592770651/wench.mp4';
 const dogeBonk = 'https://cdn.discordapp.com/attachments/419976078321385473/736559596155699290/fetchimage.png';
-const underArrest = 'https://cdn.discordapp.com/attachments/642037173729624104/775640344217649182/hornyposting.mp4';
-const bonkTwo = 'https://cdn.discordapp.com/attachments/702091543514710027/784568967208763402/EobLiWqXUAAa8jf.png';
 
-const links = [dogeBonk, dogeHorny, underArrest, bonkTwo];
+const links = [dogeBonk, dogeHorny];
 
 export const command: Command = {
   name: 'Horny',
