74cc184248e7b577f3039f58b80c3dee2e36a553
kira0x1 <kira.eva@protonmail.com>
2021-05-03 07:40:55 +0300

    wip

--- src/commands/music/autoqueue.ts
+++ src/commands/music/autoqueue.ts
@@ -1,4 +1,9 @@
+import chalk from 'chalk';
+import { GuildMember } from 'discord.js';
+import { logger } from '../../app';
 import { Command } from '../../classes/Command';
+import { getTarget } from '../../util/discordUtil';
+import { randomNumber, getPlayer } from '../../util/musicUtil';
 
 export const command: Command = {
    name: 'AutoQueue',
@@ -7,6 +12,31 @@ export const command: Command = {
    args: false,
 
    async execute(message, args) {
-      //TODO
+      const player = getPlayer(message);
+
+      let vc = message.member.voice.channel;
+
+      if (!vc && player.testVc) {
+         vc = player.testVc;
+         player.join(message);
+      }
+
+      const nonbot = vc.members.filter(m => !m.user.bot);
+
+      const chosen: GuildMember[] = [];
+
+      const ceil = Math.min(nonbot.size, 5);
+
+      for (let i = 0; i < ceil; i++) {
+         const rand = randomNumber(0, ceil);
+         chosen.push(nonbot[rand()]);
+      }
+
+      const targets = chosen.map(m => getTarget(message, m.id));
+      await Promise.all(targets);
+
+      targets.map((t, i) => {
+         logger.info(chalk.bgRed.bold(`Target ${i + 1}: ${t}`));
+      });
    }
 };
