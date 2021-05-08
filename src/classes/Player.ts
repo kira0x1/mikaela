f0013260189615e2053f4380c4617c494cf4a6cf
kira0x1 <kira.eva@protonmail.com>
2021-05-08 09:44:52 +0300

    Updated progressbar

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -8,7 +8,7 @@ import { quickEmbed } from '../util/styleUtil';
 import { Queue } from './Queue';
 import { Song } from './Song';
 import { args, coders_club_id, isProduction } from '../config';
-import createBar from 'string-progressbar';
+import progressbar from 'string-progressbar';
 
 const minVolume: number = 0.05;
 const maxVolume: number = 10;
@@ -209,7 +209,7 @@ export class Player {
       }
 
       const current = streamTime;
-      const songBar = createBar(total, current, 20)[0];
+      const songBar = progressbar.splitBar(total, current, 20)[0];
 
       return songBar;
    }
