a62d8b625efbe8dd2badeb439bf23cc3fe24d791
kira0x1 <kira.eva@protonmail.com>
2021-05-29 11:18:55 +0300

    Implemented blocking subgroup to block members from using the bot

---
+++ src/commands/blocked/unblock.ts
@@ -0,0 +1,26 @@
+import { Command } from '../../classes/Command';
+import { blockedUsers, UnBlock } from '../../database/models/Blocked';
+import { getTarget } from '../../util/discordUtil';
+import { quickEmbed, createFooter } from '../../util/styleUtil';
+
+export const command: Command = {
+   name: 'unblock',
+   description: 'unblock a user so they can use mikaela again',
+   perms: ['mod', 'kira'],
+   args: true,
+
+   async execute(message, args) {
+      const query = args.join(' ');
+      const target = await getTarget(message, query);
+      if (!target && !blockedUsers.has(query)) return quickEmbed(message, `Member \"${query}\" not found`);
+
+      const id = target?.id || query;
+
+      if (!blockedUsers.has(id))
+         return quickEmbed(message, `Member \"${target?.tag || `<@${id}>`}\" is not blocked`);
+
+      UnBlock(id);
+      const embed = createFooter(message).addField(`Unblocked`, `<@${id}>`);
+      message.channel.send(embed);
+   }
+};
---
+++ src/commands/info/blocked.ts
@@ -0,0 +1,10 @@
+import { CommandInfo } from '../../classes/CommandInfo';
+
+export const info: CommandInfo = {
+   name: 'Blocked',
+   description: 'Block users from using bot commands',
+   commands: [],
+   aliases: [],
+   override: 'list',
+   perms: ['mod', 'kira']
+};
