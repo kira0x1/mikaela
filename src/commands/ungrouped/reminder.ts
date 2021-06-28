1ddd37929aa81923f594f5ba5d4853a19e4378e1
kira0x1 <kira.eva@protonmail.com>
2021-06-28 19:53:17 +0300

    set max reminder time to 3w

--- src/commands/ungrouped/reminder.ts
+++ src/commands/ungrouped/reminder.ts
@@ -4,7 +4,7 @@ import { Command } from '../../classes/Command';
 import { createReminder } from '../../database/api/reminderApi';
 import { IReminder } from '../../database/models/Reminders';
 import { sendArgsError } from '../../util/commandUtil';
-import { createFooter } from '../../util/styleUtil';
+import { createFooter, quickEmbed } from '../../util/styleUtil';
 
 export const command: Command = {
    name: 'Reminder',
@@ -22,6 +22,10 @@ export const command: Command = {
 export async function setReminder(message: Message, time: string, content: string) {
    // If user didnt give a message then tell the user the usage of the command
    if (!time) return sendArgsError(command, message);
+   const msTime = ms(time);
+
+   if (msTime > ms('3w'))
+      return quickEmbed(message, `Time cannot exceed 3 weeks`, { addDeleteCollector: true });
 
    let description = `remind ${message.author.username} `;
    if (content) description += `to ${content} `;
@@ -33,7 +37,6 @@ export async function setReminder(message: Message, time: string, content: strin
    // Send embed telling the user that the reminder was created
    message.channel.send(embed);
 
-   const msTime = ms(time);
    let dbReminder: IReminder | undefined;
 
    if (msTime > ms('1h')) {
