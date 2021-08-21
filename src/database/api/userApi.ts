55acdd09789778022f55ca7c8888bc2ae5b95165
kira0x1 <kira.eva@protonmail.com>
2021-08-21 13:59:32 +0300

    Fixed bug when trying to use the delete reaction on favorites embed

--- src/database/api/userApi.ts
+++ src/database/api/userApi.ts
@@ -42,7 +42,11 @@ export async function addFavoriteToUser(member: DiscordUser, song: Song, message
    const user = await findOrCreate(member);
 
    if (user.favorites?.find(fav => fav.id === song.id)) {
-      return quickEmbed(message, `Sorry **${user.username}** You already have this song as a favorite`);
+      quickEmbed(message, `Sorry **${user.username}** You already have this song as a favorite`, {
+         deleteCollectorOwnerId: member.id,
+         deleteOriginalMessage: false
+      });
+      return;
    }
 
    quickEmbed(message, `**${user.username}** added song **${song.title}** to their favorites!`);
