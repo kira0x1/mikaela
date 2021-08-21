145fea78ae54a6f540f466b39c4e62da4abdf898
kira0x1 <kira.eva@protonmail.com>
2021-08-21 07:53:22 +0300

    Change eminem command  name to fortnight
    also chagned command description

--- src/commands/fun/fortnight.ts
+++ src/commands/fun/fortnight.ts
@@ -4,9 +4,9 @@ const videoLink =
    'https://cdn.discordapp.com/attachments/702091543514710027/820642367203573770/victoryroyale.mp4';
 
 export const command: Command = {
-   name: 'Eminem',
-   description: 'Eminem rapping 🐱',
-   aliases: ['victoryroyale', 'fortnight', 'tomatotown', 'bullet', 'hugh', 'hughmungus'],
+   name: 'fortnight',
+   description: 'Eminem rapping fortnight victory royale song🐱',
+   aliases: ['victoryroyale', 'tomatotown', 'bullet', 'hugh', 'hughmungus'],
 
    execute(message, args) {
       message.channel.send(videoLink);
