9b08b7457bdb3646ca05917a88e0de6ea15bd30c
kira0x1 <kira.eva@protonmail.com>
2021-09-12 07:54:06 +0300

    Add alias to playlists group

--- src/commands/info/playlist.ts
+++ src/commands/info/playlist.ts
@@ -4,6 +4,6 @@ export const info: CommandInfo = {
    name: 'Playlists',
    description: 'Users playlists',
    commands: [],
-   aliases: ['pl', 'plist'],
+   aliases: ['pl', 'plist', 'playlist'],
    override: 'list'
 };
