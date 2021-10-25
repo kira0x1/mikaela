a87355626b7e68327b012f62510fa870d2ad4112
kira0x1 <kira.eva@protonmail.com>
2021-10-25 04:28:17 +0300

    Paginate logs

--- src/commands/admin/botinfo.ts
+++ src/commands/admin/botinfo.ts
@@ -1,16 +1,86 @@
 import { Command } from '../../classes/Command';
 import { addCodeField, createFooter } from '../../util/styleUtil';
+import { Collection, Constants, MessageReaction, User } from 'discord.js';
+import { createDeleteCollector } from '../../util';
+import ms from 'ms';
+import { logger } from '../../app';
 
 export const command: Command = {
    name: 'BotInfo',
    description: 'Display bot info and stats',
    perms: ['kira'],
 
-   execute(message, args) {
+   async execute(message, args) {
       const guilds = message.client.guilds.cache;
-      const embed = createFooter(message).setTitle(`Info\nServers: ${guilds.size}`);
 
-      addCodeField(embed, '---Servers---\n\n' + guilds.array().join('\n'));
-      message.channel.send(embed);
+      const pageSize = 20;
+
+      const pages: Collection<number, string[]> = new Collection();
+      pages.set(0, []);
+      let count = 0;
+      let pageAt = 0;
+
+      for (const { '1': guild } of guilds) {
+         if (count >= pageSize) {
+            count = 0;
+            pageAt++;
+            pages.set(pageAt, []);
+         }
+
+         const pageGuilds = pages.get(pageAt);
+         if (pageGuilds) pageGuilds.push(guild.name);
+         count++;
+      }
+
+      const embed = createFooter(message).setTitle(
+         `Info\nServers: ${guilds.size}\n**Page 1 / ${pages.size}**`
+      );
+
+      addCodeField(embed, '---Servers---\n\n' + pages.get(0).join('\n'));
+
+      const msg = await message.channel.send(embed);
+
+      // If there are only 1 or none pages then dont add the next, previous page emojis / collector
+      if (pages.size <= 1) {
+         createDeleteCollector(msg, message);
+         return;
+      }
+
+      msg.react('⬅')
+         .then(() => msg.react('➡'))
+         .finally(() => createDeleteCollector(msg, message));
+
+      const filter = (reaction: MessageReaction, userReacted: User) => {
+         return (reaction.emoji.name === '➡' || reaction.emoji.name === '⬅') && !userReacted.bot;
+      };
+
+      const collector = msg.createReactionCollector(filter, { time: ms('1h') });
+
+      let currentPage = 0;
+
+      collector.on('collect', async (reaction: MessageReaction, userReacted: User) => {
+         if (reaction.emoji.name === '➡') {
+            currentPage++;
+            if (currentPage >= pages.size) currentPage = 0;
+         } else if (reaction.emoji.name === '⬅') {
+            currentPage--;
+            if (currentPage < 0) currentPage = pages.size - 1;
+         }
+
+         reaction.users.remove(userReacted);
+
+         const newEmbed = createFooter(message).setTitle(
+            `Info\nServers: ${guilds.size}\n**Page ${currentPage + 1} / ${pages.size}**`
+         );
+
+         addCodeField(newEmbed, '---Servers---\n\n' + pages.get(currentPage).join('\n'));
+         msg.edit(newEmbed);
+      });
+
+      collector.on('end', collected => {
+         msg.reactions.removeAll().catch(error => {
+            if (error.code !== Constants.APIErrors.UNKNOWN_MESSAGE) logger.error(error);
+         });
+      });
    }
 };
--- src/commands/admin/players.ts
+++ src/commands/admin/players.ts
@@ -1,6 +1,8 @@
+import { Constants, MessageReaction, User } from 'discord.js';
 import ms from 'ms';
+import { logger } from '../../app';
 import { Command } from '../../classes/Command';
-import { players } from '../../util/musicUtil';
+import { createDeleteCollector, players } from '../../util/musicUtil';
 import { addCodeField, createFooter } from '../../util/styleUtil';
 
 export const command: Command = {
@@ -9,11 +11,10 @@ export const command: Command = {
    aliases: ['streams'],
    perms: ['kira'],
 
-   execute(message, args) {
-      const embed = createFooter(message).setTitle(`Players: ${players.size}`);
+   async execute(message, args) {
       const playing = players.filter(p => p.isPlaying).array();
 
-      let content = '';
+      const pages: string[] = [];
 
       for (const player of playing) {
          const songDuration = player.currentlyPlaying?.duration.duration;
@@ -27,17 +28,69 @@ export const command: Command = {
          const songStreamTime = ms(streamTime, { long: true });
 
          const queueLength = player.queue.songs.length;
+
          let field = `server: ${player.guild.name}\nchannel: ${player.voiceChannel.name}\n`;
          field += `queue: ${queueLength}\n`;
          field += `current: ${player.currentlyPlaying?.title}\n duration: ${songStreamTime} / ${songDuration}\n`;
          field += `-----------------------------\n\n`;
-         content += field;
+         pages.push(field);
       }
 
-      if (playing.length > 0) addCodeField(embed, '---Playing---\n\n' + content);
-
       const notPlayingLength = players.filter(p => !p.isPlaying).size;
-      addCodeField(embed, `---Not Playing---\n\nPlayers: ${notPlayingLength}`);
-      message.channel.send(embed);
+
+      const embed = createFooter(message).setTitle(
+         `Players: ${players.size}\n**Page 1 / ${pages.length + 1}**`
+      );
+
+      embed.setDescription(`Playing: ${playing.length}\nNot Playing: ${notPlayingLength}`);
+
+      if (pages.length > 0) addCodeField(embed, pages[0]);
+
+      const msg = await message.channel.send(embed);
+
+      // If there are only 1 or none pages then dont add the next, previous page emojis / collector
+      if (pages.length <= 1) {
+         createDeleteCollector(msg, message);
+         return;
+      }
+
+      msg.react('⬅')
+         .then(() => msg.react('➡'))
+         .finally(() => createDeleteCollector(msg, message));
+
+      const filter = (reaction: MessageReaction, userReacted: User) => {
+         return (reaction.emoji.name === '➡' || reaction.emoji.name === '⬅') && !userReacted.bot;
+      };
+
+      const collector = msg.createReactionCollector(filter, { time: ms('1h') });
+
+      let currentPage = 0;
+
+      collector.on('collect', async (reaction: MessageReaction, userReacted: User) => {
+         if (reaction.emoji.name === '➡') {
+            currentPage++;
+            if (currentPage >= pages.length) currentPage = 0;
+         } else if (reaction.emoji.name === '⬅') {
+            currentPage--;
+            if (currentPage < 0) currentPage = pages.length - 1;
+         }
+
+         reaction.users.remove(userReacted);
+
+         const newEmbed = createFooter(message).setTitle(
+            `Players: ${players.size}\n**Page ${currentPage + 1} / ${pages.length + 1}**`
+         );
+
+         newEmbed.setDescription(`Playing: ${playing.length}\nNot Playing: ${notPlayingLength}`);
+
+         addCodeField(newEmbed, pages[currentPage]);
+         msg.edit(newEmbed);
+      });
+
+      collector.on('end', collected => {
+         msg.reactions.removeAll().catch(error => {
+            if (error.code !== Constants.APIErrors.UNKNOWN_MESSAGE) logger.error(error);
+         });
+      });
    }
 };
