676a8711f92451da08530c3acc1107a1c77d2156
kira0x1 <kira.eva@protonmail.com>
2021-11-18 00:14:00 +0300

    Fix disconnect bug

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -280,19 +280,6 @@ export class Player {
       // Create connection
       const connection = getVoiceConnection(this.guild.id);
 
-      connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
-         try {
-            await Promise.race([
-               entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
-               entersState(connection, VoiceConnectionStatus.Connecting, 5_000)
-            ]);
-            // Seems to be reconnecting to a new channel - ignore disconnect
-         } catch (error) {
-            // Seems to be a real disconnect which SHOULDN'T be recovered from
-            connection.destroy();
-         }
-      });
-
       try {
          const opusStream = ytdl(song.url, {
             filter: 'audioonly',
