b623b1bb9dd6f36ca5eb6fe930fa35d43662bd84
kira0x1 <kira.eva@protonmail.com>
2021-11-17 20:07:43 +0300

    Fixed bug with scheduled reminders

--- src/system/agenda.ts
+++ src/system/agenda.ts
@@ -3,10 +3,11 @@ import ms from 'ms';
 import { client } from '../app';
 import * as config from '../config';
 
-const agenda = new Agenda({ db: { address: config.dbURI }, processEvery: '5 seconds' });
+const agenda = new Agenda({ db: { address: config.dbURI }, processEvery: '30 seconds' });
 
 agenda.define('remind', { priority: 10, concurrency: 10 }, async (job: Job) => {
-   const { userId, content }: { userId?: string; content?: string } = job.attrs.data;
+   const { userId, botId, content }: { userId?: string; botId?: string; content?: string } = job.attrs.data;
+   if (botId !== client.user.id) return;
    const target = await client.users.fetch(userId);
    await target.send(content);
 });
@@ -19,12 +20,9 @@ export async function stopAgenda() {
    await agenda.stop();
 }
 
-export function createReminderJob(userId: string, message: string, remindIn: number) {
-   agenda.schedule(ms(remindIn, { long: true }), 'remind', { userId, message });
-}
-
 export async function createRepeatingReminderJob(userId: string, content: string, remindEvery: number) {
-   const remindJob = agenda.create('remind', { userId, content });
+   const botId = client.user.id;
+   const remindJob = agenda.create('remind', { userId, botId, content });
    await remindJob.repeatEvery(ms(remindEvery, { long: true })).save();
 }
 
