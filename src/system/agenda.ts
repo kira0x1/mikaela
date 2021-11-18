6e8e7fa7b69b5d295de09b45aa681c1f158fd3b2
kira0x1 <kira.eva@protonmail.com>
2021-11-19 00:43:58 +0300

    SkipImmediate reminder on job creation

--- src/system/agenda.ts
+++ src/system/agenda.ts
@@ -3,7 +3,7 @@ import ms from 'ms';
 import { client } from '../app';
 import * as config from '../config';
 
-const agenda = new Agenda({ db: { address: config.dbURI }, processEvery: '30 seconds' });
+const agenda = new Agenda({ db: { address: config.dbURI }, processEvery: '5 seconds' });
 
 agenda.define('remind', { priority: 10, concurrency: 10 }, async (job: Job) => {
    const { userId, botId, content }: { userId?: string; botId?: string; content?: string } = job.attrs.data;
@@ -23,7 +23,7 @@ export async function stopAgenda() {
 export async function createRepeatingReminderJob(userId: string, content: string, remindEvery: number) {
    const botId = client.user.id;
    const remindJob = agenda.create('remind', { userId, botId, content });
-   await remindJob.repeatEvery(ms(remindEvery, { long: true })).save();
+   await remindJob.repeatEvery(ms(remindEvery, { long: true }), { skipImmediate: true }).save();
 }
 
 export async function getJobsById(userId: string) {
