09d4d85f7700b1810666f0b00ecb4848fedb4884
kira0x1 <kira.eva@protonmail.com>
2021-11-19 23:38:07 +0300

    Add slash interaction for the play command

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -1,6 +1,8 @@
+import { SlashCommandStringOption } from '@discordjs/builders';
 import { Message } from 'discord.js';
-import { Command } from '../../classes/Command';
-import { onSongRequest, getPlayer } from '../../util/musicUtil';
+import { Command, instanceOfSong, Song } from '../../classes';
+import { getSong, playSongFromInteraction } from '../../util';
+import { getPlayer, onSongRequest } from '../../util/musicUtil';
 
 export const command: Command = {
    name: 'play',
@@ -8,6 +10,13 @@ export const command: Command = {
    aliases: ['p'],
    usage: '[url | search]',
    args: false,
+   interactionOptions: [
+      new SlashCommandStringOption()
+         .setName('song')
+         .setDescription('searches youtube, and spotify for the song')
+         .setRequired(true)
+   ],
+   hasInteraction: true,
 
    async execute(message: Message, args: string[]) {
       // if no arguments given then attempt to play whats in the queue
@@ -18,5 +27,31 @@ export const command: Command = {
 
       // If there were arguments, then process the input and play the song
       onSongRequest(message, args, this);
+   },
+
+   async executeInteraction(interaction) {
+      const value = interaction.options.data[0]?.value;
+
+      if (!value) {
+         return;
+      }
+
+      // Search for song
+      const res = await getSong(value.toString(), true);
+
+      if (!res) {
+         interaction.reply(`Song: ${value} not found`);
+         return;
+      }
+
+      let song: Song;
+
+      if (res instanceof Array) {
+         song = res[0];
+      } else if (instanceOfSong(res)) {
+         song = res;
+      }
+
+      playSongFromInteraction(interaction, song);
    }
 };
