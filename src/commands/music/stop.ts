1c00917bf1c7e41160045275b8f01b51d217fa9b
kira0x1 <kira.eva@protonmail.com>
2021-11-19 23:38:13 +0300

    add slash interaction for the stop command

--- src/commands/music/stop.ts
+++ src/commands/music/stop.ts
@@ -5,11 +5,21 @@ export const command: Command = {
    name: 'stop',
    description: 'stops the music player',
    aliases: ['end', 's', 'dc', 'disconnect', 'leave', 'quit'],
+   hasInteraction: true,
 
    execute(message, args) {
       const player = getPlayer(message.guildId);
       if (player) {
          player.leave();
       }
+   },
+
+   executeInteraction(interaction) {
+      const player = getPlayer(interaction.guildId);
+      if (player) {
+         player.leave();
+      }
+
+      interaction.reply('Stopped playing');
    }
 };
